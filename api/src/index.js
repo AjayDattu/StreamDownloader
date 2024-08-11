ad90b7f6f5f2ac1d8ec85ea8caae57f7b7f774e5
Ajay Dattu <dattuajay005@gmil.com>
2024-08-11 20:30:44 +0530

    completed backend

--- api/src/index.js
+++ api/src/index.js
@@ -1,43 +1,30 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const axios = require('axios');
+const ytdl = require('ytdl-core');
+const cors = require('cors');
+require('dotenv').config();
+
+const port = process.env.PORT || 4000;
 
 const app = express();
-const PORT = 4000;
-const LANGUAGE_VERSIONS = {
-  "python3": "3.9.7",    // Example version
-  "javascript": "14.17.6",
-  "java": "17",
-  "c": "11",
-  "ruby": "3.0.2",
-  "shell": "5.8",
-  // Add more languages and versions as needed
-};
-app.use(bodyParser.json());
+app.use(cors());
 
-const API = axios.create({
-  baseURL: 'https://emkc.org/api/v2/piston',
+app.get('/download', async (req, res) => {
+    try {
+        const url = req.query.url;
+        const videoId = await ytdl.getURLVideoID(url);
+        const metaInfo = await ytdl.getInfo(url);
+
+        let data = {
+            url: 'https://www.youtube.com/embed/' + videoId,
+            info: metaInfo.formats
+        };
+
+        return res.send(data);
+    } catch (error) {
+        return res.status(500).send('Error fetching video information');
+    }
 });
 
-app.post('/execute', async (req, res) => {
-  const { language, sourceCode } = req.body;
-
-  try {
-    const response = await API.post('/execute', {
-      language: language,
-      version: LANGUAGE_VERSIONS[language],
-      files: [
-        {
-          content: sourceCode,
-        },
-      ],
-    });
-    res.json(response.data);
-  } catch (error) {
-    res.status(500).json({ error: 'Failed to execute code', details: error.message });
-  }
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+    console.log(`Server is running on PORT: ${port}`);
 });
