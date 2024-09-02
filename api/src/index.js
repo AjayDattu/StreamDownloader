db34cc6da04b6711e5c5049fecba301eb625ce0e
Ajay Dattu <dattuajay005@gmail.com>
2024-09-02 21:31:18 +0530

    Completed Backend

--- api/src/index.js
+++ api/src/index.js
@@ -1,36 +1,31 @@
 const express = require('express');
 const axios = require('axios');
+
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.use(express.json()); // for parsing application/json
-
-app.post('/get-video-details', async (req, res) => {
-  const { videoId } = req.body;
-
-  if (!videoId) {
-    return res.status(400).send('Video ID is required');
-  }
-
+app.get('/video-details', async (req, res) => {
   const options = {
     method: 'GET',
     url: 'https://youtube-media-downloader.p.rapidapi.com/v2/video/details',
-    params: { videoId },
+    params: {
+      videoId: req.query.videoId || 'G33j5Qi4rE8', // Use query parameter or default ID
+    },
     headers: {
       'x-rapidapi-key': 'f19d73c296msh4416db469383f01p1bec96jsnc051ff7e2d7c',
-      'x-rapidapi-host': 'youtube-media-downloader.p.rapidapi.com'
-    }
+      'x-rapidapi-host': 'youtube-media-downloader.p.rapidapi.com',
+    },
   };
 
   try {
     const response = await axios.request(options);
-    res.json(response.data);
+    res.json(response.data); // Send the response back to the client
   } catch (error) {
     console.error(error);
-    res.status(500).send('Error retrieving video details');
+    res.status(500).send('An error occurred while fetching video details');
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
